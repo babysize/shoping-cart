3fb534ffb6b860c0853b05ab20d5f68ccefdb351
Natalia Shipil <nshipil91@gmail.com>
2023-01-19 16:59:41 +0900

    change icon cart to count product

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ButtonBase from '@mui/material/ButtonBase';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import Box from '@mui/material/Box';
+import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
 
 const Img = styled('img')({
   margin: 'auto',
@@ -22,11 +22,11 @@ export default function ComplexGrid(props) {
     <Paper
       sx={{
         p: 2,
-        margin: 'auto',
-        maxWidth: 400,
+        margin: 0.5,
+        // maxWidth: 400,
         flexGrow: 1,
-        backgroundColor: (theme) =>
-          theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        border: 1,
+        borderColor: 'divider'
       }}
     >
       <Grid container spacing={2}>
@@ -35,23 +35,33 @@ export default function ComplexGrid(props) {
             <Img alt="complex" src={props.path}/>
           </ButtonBase>
         </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item xs container direction="column" spacing={2}>
+        <Grid item xs={12} sm container direction="column">
+          <Grid item xs container direction="row">
             <Grid item xs>
               <Typography gutterBottom variant="subtitle1" component="div">
                 {props.name}
               </Typography>
             </Grid>
-            <Stack direction="row" spacing={1}>
-            <IconButton color="primary" aria-label="add to shopping cart">
-              <AddShoppingCartIcon />
-            </IconButton>
-            </Stack>
+            <Grid item>
+              <Typography variant="subtitle1" component="div">
+                {props.coast} $
+              </Typography>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Typography variant="subtitle1" component="div">
-              {props.coast} $
-            </Typography>
+          <Grid item xs container alignItems='end'>
+            <Grid item>
+              <IconButton onClick={()=>{props.onClick(props)}} 
+                          sx={{display: props.count? 'none' : 'block',
+                               }} 
+                          color="primary" aria-label="add to shopping cart">
+                <AddShoppingCartIcon />
+              </IconButton>
+            </Grid>
+            <Grid item sx={{display: props.count? 'block' : 'none',p: 1}}>
+                <Typography gutterBottom variant="subtitle1" component="div" margin={0}>
+                  {props.count} шт.
+                </Typography>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
@@ -59,13 +69,7 @@ export default function ComplexGrid(props) {
   );
 }
 
-// export default function FullWidthGrid() {
-//     return (
-//       <Box sx={{ flexGrow: 1 }}>
-//         {props.cont}
-//       </Box>
-//     )
-// }
+
 
 class ProductList extends React.Component {
     constructor(props) {
@@ -73,71 +77,94 @@ class ProductList extends React.Component {
         this.state = {
           products: [
             {
+              id: '#1',
               name:'Carrot',
               path: "images/products/carrot.jpg",
               coast: 4.99,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#2',
               name:'Tomato',
               path: "images/products/tomato.jpeg",
               coast: 9.49,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#3',
               name:'Potato',
               path: "images/products/potato.jpeg",
               coast: 1.99,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#4',
               name:'Cucumber',
               path: "images/products/cucumber.jpeg",
               coast: 2.49,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#5',
               name:'Broccoli',
               path: "images/products/broccoli.jpeg",
               coast: 21.99,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#6',
               name:'Onion',
               path: "images/products/onion.jpeg",
               coast: 0.99,
-              isAdd: false,
+              count: 0,
             },
             {
+              id: '#7',
               name:'Pumpkin',
               path: "images/products/pumpkin.jpeg",
               coast: 11.49,
-              isAdd: false,
+              count: 0,
             }
-          ]
+          ],
+          cart: []
         }
     }
 
+    handleClick(product) {
+      const products = this.state.products.map(p =>
+        p.name === product.name
+          ? {...p, count: 1} 
+          : p)
+      this.setState({
+        cart: [...this.state.cart, {...product,...{ count: 1}}],
+        products: products
+      })
+    }
+
     render() {
-        const content = this.state.products.map((product) =>
+        const products = this.state.products.map((product) =>
           <ComplexGrid
+            key={product.id}
             name={product.name}
             path={product.path}
-            coast={product.coast}
-            isAdd={product.isAdd}
+            coast={product.coast} 
+            count={product.count}
+            onClick={product => this.handleClick(product)}
           />
         )
 
         return (
-            <div>
-                {content}
-            </div>
+          <Grid container spacing={3} 
+                alignItems="flex-start" justifyContent="space-between">
+            <Grid item xs={6}>
+              {products}
+            </Grid>
+            <IconButton sx={{marginTop: 3}} 
+                        color="primary" aria-label="open shopping cart">
+              <ShoppingBasketOutlinedIcon/>
+            </IconButton>
+          </Grid>
         )
-        // return (
-        //   <FullWidthGrid
-        //     cont={content}
-        //   />
-        // )
     }
 }
 
