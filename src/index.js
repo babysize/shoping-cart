1f2d15de1d3bf7480124f6fb21b5330014d34e92
Natalia Shipil <nshipil91@gmail.com>
2023-01-27 01:12:43 +0900

    remake product cart

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ButtonBase from '@mui/material/ButtonBase';
 import IconButton from '@mui/material/IconButton';
-import Stack from '@mui/material/Stack';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import ShoppingBasketOutlinedIcon from '@mui/icons-material/ShoppingBasketOutlined';
+import Stack from '@mui/material/Stack';
 
 const Img = styled('img')({
   margin: 'auto',
@@ -17,32 +17,30 @@ const Img = styled('img')({
   maxHeight: '100%',
 });
 
-export default function ComplexGrid(props) {
+export default function ProductCart(props) {
   return (
-    <Paper
+    <Grid item xs={12} sm={6} md={4}>
+    <Paper 
       sx={{
         p: 2,
-        margin: 0.5,
-        // maxWidth: 400,
-        flexGrow: 1,
         border: 1,
         borderColor: 'divider'
       }}
     >
-      <Grid container spacing={2}>
+      <Grid container>
         <Grid item>
-          <ButtonBase sx={{ width: 128, height: 128 }}>
+          <ButtonBase sx={{ width: 130, height: 130 }}>
             <Img alt="complex" src={props.path}/>
           </ButtonBase>
         </Grid>
-        <Grid item xs={12} sm container direction="column">
+        <Grid item xs sm container direction="column">
           <Grid item xs container direction="row">
-            <Grid item xs>
+            <Grid item xs={7}>
               <Typography gutterBottom variant="subtitle1" component="div">
                 {props.name}
               </Typography>
             </Grid>
-            <Grid item>
+            <Grid item xs={5} sx={{textAlign:'end'}}>
               <Typography variant="subtitle1" component="div">
                 {props.coast} $
               </Typography>
@@ -51,13 +49,13 @@ export default function ComplexGrid(props) {
           <Grid item xs container alignItems='end'>
             <Grid item>
               <IconButton onClick={()=>{props.onClick(props)}} 
-                          sx={{display: props.count? 'none' : 'block',
-                               }} 
+                          sx={{display: props.count? 'none' : 'block'}} 
                           color="primary" aria-label="add to shopping cart">
                 <AddShoppingCartIcon />
               </IconButton>
             </Grid>
-            <Grid item sx={{display: props.count? 'block' : 'none',p: 1}}>
+            <Grid item sx={{display: props.count? 'block' : 'none',
+                            p: 1}}>
                 <Typography gutterBottom variant="subtitle1" component="div" margin={0}>
                   {props.count} шт.
                 </Typography>
@@ -66,107 +64,121 @@ export default function ComplexGrid(props) {
         </Grid>
       </Grid>
     </Paper>
+    </Grid>
   );
 }
 
-
-
 class ProductList extends React.Component {
-    constructor(props) {
-      super(props)
-        this.state = {
-          products: [
-            {
-              id: '#1',
-              name:'Carrot',
-              path: "images/products/carrot.jpg",
-              coast: 4.99,
-              count: 0,
-            },
-            {
-              id: '#2',
-              name:'Tomato',
-              path: "images/products/tomato.jpeg",
-              coast: 9.49,
-              count: 0,
-            },
-            {
-              id: '#3',
-              name:'Potato',
-              path: "images/products/potato.jpeg",
-              coast: 1.99,
-              count: 0,
-            },
-            {
-              id: '#4',
-              name:'Cucumber',
-              path: "images/products/cucumber.jpeg",
-              coast: 2.49,
-              count: 0,
-            },
-            {
-              id: '#5',
-              name:'Broccoli',
-              path: "images/products/broccoli.jpeg",
-              coast: 21.99,
-              count: 0,
-            },
-            {
-              id: '#6',
-              name:'Onion',
-              path: "images/products/onion.jpeg",
-              coast: 0.99,
-              count: 0,
-            },
-            {
-              id: '#7',
-              name:'Pumpkin',
-              path: "images/products/pumpkin.jpeg",
-              coast: 11.49,
-              count: 0,
-            }
-          ],
-          cart: []
-        }
-    }
-
-    handleClick(product) {
-      const products = this.state.products.map(p =>
-        p.name === product.name
-          ? {...p, count: 1} 
-          : p)
-      this.setState({
-        cart: [...this.state.cart, {...product,...{ count: 1}}],
-        products: products
-      })
-    }
 
     render() {
-        const products = this.state.products.map((product) =>
-          <ComplexGrid
+        const products = this.props.products.map((product) =>
+          <ProductCart
             key={product.id}
             name={product.name}
             path={product.path}
             coast={product.coast} 
             count={product.count}
-            onClick={product => this.handleClick(product)}
+            onClick={() => this.props.onClick(product)}
           />
         )
 
         return (
-          <Grid container spacing={3} 
-                alignItems="flex-start" justifyContent="space-between">
-            <Grid item xs={6}>
-              {products}
-            </Grid>
-            <IconButton sx={{marginTop: 3}} 
-                        color="primary" aria-label="open shopping cart">
-              <ShoppingBasketOutlinedIcon/>
-            </IconButton>
+           <Grid container spacing={1}>
+            {products}
           </Grid>
         )
     }
 }
 
+class ShoppingCart extends React.Component {
+  
+}
+
+class Shop extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      products: [
+        {
+          id: '#1',
+          name:'Carrot',
+          path: "images/products/carrot.jpg",
+          coast: 4.99,
+          count: 0,
+        },
+        {
+          id: '#2',
+          name:'Tomato',
+          path: "images/products/tomato.jpeg",
+          coast: 9.49,
+          count: 0,
+        },
+        {
+          id: '#3',
+          name:'Potato',
+          path: "images/products/potato.jpeg",
+          coast: 1.99,
+          count: 0,
+        },
+        {
+          id: '#4',
+          name:'Cucumber',
+          path: "images/products/cucumber.jpeg",
+          coast: 2.49,
+          count: 0,
+        },
+        {
+          id: '#5',
+          name:'Broccoli',
+          path: "images/products/broccoli.jpeg",
+          coast: 21.99,
+          count: 0,
+        },
+        {
+          id: '#6',
+          name:'Onion',
+          path: "images/products/onion.jpeg",
+          coast: 0.99,
+          count: 0,
+        },
+        {
+          id: '#7',
+          name:'Pumpkin',
+          path: "images/products/pumpkin.jpeg",
+          coast: 11.49,
+          count: 0,
+        }
+        ],
+      cart: [],
+    }
+  }
+
+  handleClick(product) {
+    const products = this.state.products.map(p =>
+      p.name === product.name
+        ? {...p, count: 1} 
+        : p)
+    this.setState({
+      cart: [...this.state.cart, {...product,...{ count: 1}}],
+      products: products
+    })
+  }
+
+  render () {
+
+    return(
+    <div>
+      <IconButton sx={{margin: 2}} 
+                  color="primary" aria-label="open shopping cart">
+        <ShoppingBasketOutlinedIcon/>
+      </IconButton>
+      <ProductList products={this.state.products}
+                   onClick={product => this.handleClick(product)}
+      />
+    </div>
+    )
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ProductList />);
+root.render(<Shop/>);
