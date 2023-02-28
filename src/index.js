8defcf7254177a59bc3d840ed26d74efb7400573
Natalia Shipil <nshipil91@gmail.com>
2023-02-28 20:09:29 +0600

    #4 delete comments

--- src/index.js
+++ src/index.js
@@ -260,8 +260,3 @@ class Shop extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Shop/>);
-
-
-//рефакторинг:
-//  -чистые функции?
-//тест на компонент селектора товаров
